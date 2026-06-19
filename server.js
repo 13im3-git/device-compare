967a81e4ce542e9481635f140709aabb41e62264
13im3-git <13im3.git@gmail.com>
2026-06-19 09:20:03 -0700

    Fix: replace bcrypt with bcryptjs for Railway compatibility

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const ejs = require('ejs');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const fs = require('fs');
